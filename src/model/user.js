ee9f7fcd1c2c1f3db11670765a91c33fb8fbc31e
yanosh01 <36453869+yanosh01@users.noreply.github.com>
2018-03-16 11:42:42 +0100

    Add files via upload

---
+++ src/model/user.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+let Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  name: { type: String, required: true },
+  discordid: { type: String, required: true, unique: true },
+  lastpostdatetime: { type: Number }
+  // timestamp: true
+});
+
+module.exports = mongoose.model('User', userSchema);
