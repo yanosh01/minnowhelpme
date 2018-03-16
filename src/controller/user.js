99cde35e6a5bffe6d383d49dd997cbf23467fed4
yanosh01 <36453869+yanosh01@users.noreply.github.com>
2018-03-16 12:34:23 +0100

    Add files via upload

---
+++ src/controller/user.js
@@ -0,0 +1,101 @@
+import mongoose from 'mongoose';
+import User from '../model/user';
+
+let checkRegisteredUser = discordId => {
+  return User.findOne(
+    { discordid: discordId },
+    (err, user) => {
+      if (err || !!user) {
+        return false;
+      }
+
+      return true;
+    }
+  )
+    .then(data => (!!data ? true : false))
+    .catch(err => false);
+};
+
+let checkLastPost = discordId => {
+  return User.findOne(
+    { discordid: discordId },
+    (err, user) => {
+      if (err || !user) {
+        console.log('error');
+      }
+      return;
+    }
+  )
+    .then(data => {
+      if (!!data) {
+        return data.lastpostdatetime;
+      } else {
+        throw 'ERROR';
+        return;
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      return false;
+    });
+};
+
+const updateTime = (discordId, time) => {
+  return User.findOne(
+    { discordid: discordId },
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log('success');
+      user.lastpostdatetime = time;
+      let result = user
+        .save(err => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          console.log('success');
+          return;
+        })
+        .then(data => {
+          return;
+        })
+        .catch(err => {
+          return;
+        });
+      return result;
+    }
+  ).catch(err => 'Error');
+};
+
+const registration = (discordName, discordId) => {
+  let user = new User({
+    name: discordName,
+    discordid: discordId
+  });
+
+  let result = user
+    .save(err => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      return;
+    })
+    .then(data => {
+      return `created user`;
+    })
+    .catch(err => {
+      return 'ERROR';
+    });
+  return result;
+};
+
+export {
+  checkRegisteredUser,
+  checkLastPost,
+  updateTime,
+  registration
+};
